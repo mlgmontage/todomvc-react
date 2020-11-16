e7b4c312609a89de5b8251cd76e7019387d84511
mlgmontage <kettleofrassel1@gmail.com>
2020-11-16 13:04:23 +0500

    lifting up todo list state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
+import { Component } from "react";
 import TodoList from "./components/todolist";
 import TodoForm from "./components/todoform";
 
-function App() {
-  return (
-    <div className="container">
-      <header>
-        <h1 className="text-center">Todo MVC</h1>
-      </header>
-      <TodoForm />
-      <TodoList />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { text: "Watch YT", isCompleted: false },
+      { text: "Complete Todo MVC", isCompleted: true },
+      { text: "Play chess", isCompleted: false },
+    ],
+  };
+
+  complete = (index) => {
+    const todos = [...this.state.todos];
+    todos[index].isCompleted = !todos[index].isCompleted;
+    this.setState({
+      todos,
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1 className="text-center">Todo MVC</h1>
+        </header>
+        <TodoForm />
+        <TodoList todos={this.state.todos} complete={this.complete} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/todolist.js
+++ src/components/todolist.js
@@ -1,36 +1,15 @@
 import { Component } from "react";
 
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.complete = this.complete.bind(this);
-  }
-
-  complete(index) {
-    const todos = [...this.state.todos];
-    todos[index].isCompleted = !todos[index].isCompleted;
-    this.setState({
-      todos,
-    });
-  }
-
-  state = {
-    todos: [
-      { text: "Watch YT", isCompleted: false },
-      { text: "Complete Todo MVC", isCompleted: true },
-      { text: "Play chess", isCompleted: false },
-    ],
-  };
-
   render() {
     return (
       <div className="list-group col-md-6 m-auto">
-        {this.state.todos.map((todo, ind) => (
+        {this.props.todos.map((todo, ind) => (
           <div className="list-group-item" key={ind}>
             <div className="form-check">
               <input
                 className="form-check-input"
-                onChange={() => this.complete(ind)}
+                onChange={() => this.props.complete(ind)}
                 checked={todo.isCompleted}
                 type="checkbox"
                 id={ind}

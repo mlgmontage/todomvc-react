7e0dfd466c4c64e55316cde1bfa031f2d22bfcc6
mlgmontage <kettleofrassel1@gmail.com>
2020-11-16 11:53:36 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1 className="text-center">Todo MVC</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
